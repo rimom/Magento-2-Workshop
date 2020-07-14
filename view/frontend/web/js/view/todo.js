abd2b4cc107890f957435321eda27da886032aee
Rimom Costa <rimomcosta@gmail.com>
2020-07-14 01:09:24 +0000

    Lesson 16: Adding tasks to the UI Component defaults

--- view/frontend/web/js/view/todo.js
+++ view/frontend/web/js/view/todo.js
@@ -1,5 +1,15 @@
-define(['uiComponent'], (Component) => {
+define(['uiComponent'], function (Component) {
     'use strict';
 
-    return Component.extend({});
+    return Component.extend({
+        defaults: {
+            tasks: [
+                {label: "Task 1"},
+                {label: "Task 2"},
+                {label: "Task 3"},
+                {label: "Task 4"},
+                {label: "Task 5"}
+            ]
+        }
+    });
 });
